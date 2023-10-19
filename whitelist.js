708c062630283a7bef6d281f122fca29df0d162d
Eatingchic <62099603+Eatingchic@users.noreply.github.com>
2023-10-19 22:46:06 +0800

    Create whitelist.js

---
+++ whitelist.js
@@ -0,0 +1,15 @@
+/*
+*Get cloudservice of ufe for free.
+*Fill in your domain name and create a Pull Request to add your domain to the whitelist.
+*NAME : Fill in the name of your website
+*Domain : Fill in your website's domain
+*About CDN:We use a CDN for our cloud service, if you want to use a cloud service without a CDN, please add [No CDN?] at the end.Like this: "Website Name","www.example.com"//[No CDN?]
+*In order to prevent abuse, we will review your pullrequest
+*/
+
+var whitelist = {
+  "","ufe.hietstudios.net"
+  "","hietstudios.net"
+  "","www.hietstudios.net"
+
+}
